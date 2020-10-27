ab2fb1108ad411e975f4484939e9d2cc13530626
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-27 20:37:40 +0800

    [TodoList] [Christian & Hannah] add: TodoGenerator.js file

---
+++ src/components/TodoGenerator.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {v4 as uuidv4} from 'uuid';
+class TodoGenerator extends Component {
+    
+    onSubmit = (event) => {
+        event.preventDefault();
+        const text = event.target.todoInput.value;
+        const id = uuidv4();
+        const todo = { id: id, text: text, done: false };
+
+        
+        this.props.addTodo(todo);
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit = {this.onSubmit}>
+                    <span>
+                        <input type = "text" placeholder = "input a new todo here..." id="todoInput" name="todoInput"/>
+                        <input type = "submit" value="add" />
+                    </span>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default TodoGenerator;
