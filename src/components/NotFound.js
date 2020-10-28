b0dc3339d89b9746622dad1f5c364a1b44c2ecb9
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-29 01:09:05 +0800

    [TodoList] [Christian & Hannah] refactor: modified 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class NotFound extends Component {
+    componentDidMount () {
+        const script = document.createElement("script");
+    
+        script.src = "https://kit.fontawesome.com/4b9ba14b0f.js";
+        script.async = true;
+    
+        document.body.appendChild(script);
+    }
+
     render() {
         return (
-            <div>
-                404 page not found
+            <div class="mainbox">
+                <div class="err">4</div>
+                <i class="far fa-question-circle fa-spin"></i>
+                <div class="err2">4</div>
+                <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?
+                    <p>Let's go <a href="http://localhost:3000/">home</a> and try from there.</p>
+                </div>
             </div>
         );
     }
